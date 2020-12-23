ec425d57f5e1ef37bdc1dfd0b6cc62a9876e36cd
ADITYA-KUMAR-MISHRA <mishraaditya805@gmail.com>
2020-12-23 14:45:15 +0530

    added insert function and pushhead function

--- singlylinkedlist.cpp
+++ singlylinkedlist.cpp
@@ -39,6 +39,36 @@ void print(node * head){
     }
     printf("Data = %d , node = %d\n",head->data,n);
 }
+//function to insert node in singly linked list
+// n is node index
+void insert(node *head, int data,int num){
+    int i = 0;
+    for( ;i<num;++i){                                              //reach to the node
+        if(head->next == NULL) {
+            cout<<"out of bounds\n";
+            break;
+        }
+        head = head->next;
+    }
+    if(i == num){
+        node * temp;
+        temp = (node *)malloc(sizeof(node));                                    //create empty node
+        temp->next = head->next;                                                //copy next to new creted node                     
+        head->next = temp;                                                      
+        temp->data = head->data;
+        head->data = data;
+    }
+}
+//funtion to push data at head and return adress of new head
+node * pushhead(node * head, int data){
+    node * temp;
+    temp = (node *)malloc(sizeof(node));
+    temp->data = data;
+    temp->next = head;
+    return temp;
+}
+
+
 int main(){
     node * head = NULL;                             //head of the linked list
     node * tail =  NULL;                               //tail of the linked list
@@ -48,6 +78,7 @@ int main(){
     tail = pushback_t(tail,9);
     tail = pushback_h(head,69);
     tail = pushback_t(tail,50);
+    insert(head,1234,2);
+    head =  pushhead(head,92);
     print(head);
-
 }
